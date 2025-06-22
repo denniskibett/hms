fbfb50f2e602fdc5707e3c1e112621f23d2c2b49
Dennis Kibet <kibettdennis@gmail.com>
2025-06-22 19:13:39 +0300

    Initial commit for TailDash Laravel + TailAdmin

--- resources/views/basic-tables.blade.php
+++ resources/views/basic-tables.blade.php
@@ -19,12 +19,29 @@
                 </div>
                 <div class="p-5 border-t border-gray-100 dark:border-gray-800 sm:p-6" >
                   <!-- ====== Table Six Start -->
+                  @include('partials.table.table-01')
+
+                  <!-- ====== Table Six End -->
+                </div>
+              </div>
+            </div>
+<br />
+            <div class="space-y-5 sm:space-y-6">
+              <div class="rounded-2xl border border-gray-200 bg-white dark:border-gray-800 dark:bg-white/[0.03]">
+                <div class="px-5 py-4 sm:px-6 sm:py-5">
+                  <h3 class="text-base font-medium text-gray-800 dark:text-white/90">
+                    Basic Table 2
+                  </h3>
+                </div>
+                <div class="p-5 border-t border-gray-100 dark:border-gray-800 sm:p-6" >
+                  <!-- ====== Table Six Start -->
                   @include('partials.table.table-06')
 
                   <!-- ====== Table Six End -->
                 </div>
               </div>
             </div>
+          </div>
           {{-- </div> --}}
         </main>
         <!-- ===== Main Content End ===== -->
